8eece6543c8ee1efcc2a872c042636ab16b83e57
stanly-sudo <alys.git24@gmail.com>
2025-07-04 15:22:51 +0800

    importing pic from assets in card component Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,11 @@
+import profilePic from './assets/makima.jpg'
+
 function Card() {
   return (
     <>
         <div className="card">
-          <img src="..." alt="" />
+          {/* <img src="/src/assets/makima.jpg" alt="profile picture" width="200rem" height="200rem" /> */}
+          <img src={profilePic} alt="profile picture" width="200rem" height="200rem" />
           <h2>Mazaru</h2>
           <p>Studying react.</p>
         </div>
