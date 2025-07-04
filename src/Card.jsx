cfb8799b28c6008a2243b8442baa9790b26fdbdd
stanly-sudo <alys.git24@gmail.com>
2025-07-04 15:15:04 +0800

    card component in App.jsx

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+function Card() {
+  return (
+    <>
+        <div className="card">
+          <img src="..." alt="" />
+          <h2>Mazaru</h2>
+          <p>Studying react.</p>
+        </div>
+    </>
+  )
+}
+
+export default Card
