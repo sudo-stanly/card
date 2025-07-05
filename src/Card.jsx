72ac46f55747e243f124426475a49673257b0509
stanly-sudo <alys.git24@gmail.com>
2025-07-05 09:36:12 +0800

    fixed typo

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,7 +6,7 @@ function Card() {
         <div className="card">
           {/* <img src="/src/assets/makima.jpg" alt="profile picture" width="200rem" height="200rem" /> */}
           <img src={profilePic} className="card-image" alt="profile picture"/>
-          <h2 className="card-title">Mazaru</h2>
+          <h2 className="card-title">Masaru</h2>
           <p className="card-text">Studying react.</p>
         </div>
     </>
